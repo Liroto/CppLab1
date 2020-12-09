07573f0aabc124a60530f4d32c13440f40909950
Liroto <ololoshin228@gmail.com>
2020-12-10 01:20:06 +0300

    Задание работает.

--- CppLab1/CppLab1.cpp
+++ CppLab1/CppLab1.cpp
@@ -1,20 +1,55 @@
-﻿// CppLab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
+#include <array>
+using namespace std;
 
-#include <iostream>
+
+void processArray(int arr[], int n) 
+{
+	int iterator = 1;
+	int count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (i % 2 == 0)
+		{
+			arr[i] = pow(2, iterator);
+		}
+		else
+		{
+			arr[i] = pow(3, iterator);
+			iterator++;
+		}
+		if (arr[i] > 9 && arr[i] < 100) {
+			count++;
+		}
+		cout << arr[i] << endl;
+	};
+
+	cout << "There are a " << count << " two-digit numbers:" << endl;
+	int counter = 0;
+	int *result_massive = new int[count];
+	
+
+	for (int j = 0; j < n; j++)
+	{
+		if (arr[j] > 9 && arr[j] < 100) 
+		{
+			result_massive[counter] = arr[j];
+			counter++;
+		}	
+	}
+
+	for (int i = 0; i <= sizeof(result_massive)/*4*/; i++)
+	{
+		cout << result_massive[i] << endl;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	const int n = 16;
+	int nums[n] = { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 };
+
+	processArray(nums, n);
+	cout << "Main function is finished." << endl;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
